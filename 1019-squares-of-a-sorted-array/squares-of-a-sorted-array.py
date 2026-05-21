dc67bdc4c8e10713bb2735357d0305131d728254
shashank-poojary-simpleenergy <138311524+shashank-poojary-simpleenergy@users.noreply.github.com>
2026-05-21 06:16:23 +0530

    Time: 13 ms (54.47%) | Memory: 14.5 MB (8.93%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,35 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums1=[]
-        nums2=[]
-        i=0
-        j=0
         res=[]
-        
-
-        for k in range(0,len(nums)):
-            if nums[k]<0:
-                nums1.append(nums[k]**2)
-            else:
-                nums2.append(nums[k]**2)
-        nums1.reverse()
-        while i<len(nums1) and j<len(nums2):
-            if nums1[i]<nums2[j]:
-                res.append(nums1[i])
-                i=i+1
-            else:
-                res.append(nums2[j])
-                j=j+1
-        while j<len(nums2):
-            res.append(nums2[j])
-            j=j+1
-        while i<len(nums1):
-            res.append(nums1[i])
-            i=i+1
-        return res
-
-
-
-
-        
+        for i in range(len(nums)):
+            if nums[i]<=0 or nums[i]>0:
+                res.append(nums[i]**2)
+        res.sort()
+        for i in range(len(nums)):
+            nums[i]=res[i]
+        return nums
